6fe709b59e15372b6182187c7a475d4da9134929
Rahul <43100342+dev-rb@users.noreply.github.com>
2025-12-10 16:50:21 -0500

    Reorder

--- js/app/packages/macro-entity/src/components/EntityWithEverything.tsx
+++ js/app/packages/macro-entity/src/components/EntityWithEverything.tsx
@@ -47,85 +47,32 @@ import type { EntityClickEvent, EntityClickHandler } from './Entity';
 import { PropertyPills } from './PropertyPills';
 import { UnifiedListItem } from './unified-list-item';
 
-function UnreadIndicator(props: { active?: boolean }) {
-  return (
-    <div class="flex size-4 items-center justify-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        classList={{
-          'fill-accent': true,
-          'opacity-0': !props.active,
-        }}
-        viewBox="0 0 8 8"
-        width="75%"
-        height="75%"
-        fill="none"
-      >
-        <path d="M3.39622 8C3.29136 8 3.23894 7.94953 3.23894 7.84858L3.33068 5.13565L0.932129 6.58675C0.836012 6.63722 0.76174 6.6204 0.709312 6.53628L0.0801831 5.56467C0.0190178 5.47213 0.0364936 5.40063 0.132611 5.35016L2.58359 4.07571L0.09329 2.88959C-0.00282696 2.83912 -0.0246717 2.77182 0.0277557 2.6877L0.59135 1.58991C0.643778 1.49737 0.71805 1.47634 0.814167 1.52681L3.31758 2.95268L3.21272 0.151421C3.21272 0.0504735 3.26515 0 3.37 0H4.57583C4.68069 0 4.73312 0.0504735 4.73312 0.151421L4.64137 2.94006L7.14478 1.40063C7.2409 1.34175 7.3108 1.35857 7.35449 1.4511L7.97051 2.46057C8.02294 2.5531 8.00546 2.6204 7.91808 2.66246L5.40157 4L7.82633 5.18612C7.91371 5.23659 7.93556 5.30389 7.89187 5.38801L7.36759 6.4858C7.32391 6.58675 7.25837 6.60778 7.17099 6.54889L4.6938 5.13565L4.78554 7.84858C4.79428 7.94953 4.74185 8 4.62826 8H3.39622Z" />
-      </svg>
-    </div>
-  );
-}
+const createFormattedDate = (timestamp: number) =>
+  createMemo(() => {
+    if (timestamp < 1e12) {
+      timestamp *= 1000;
+    }
+    const date = new Date(timestamp);
+    const currentDate = new Date();
+    if (date.getDate() === currentDate.getDate()) {
+      return date.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+    }
+    if (date.getFullYear() === currentDate.getFullYear()) {
+      return date.toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+      });
+    }
 
-function SharedBadge(props: { ownerId: string }) {
-  return (
-    <div class="font-mono font-medium user-select-none uppercase flex items-center text-ink-extra-muted p-0.5 gap-1 text-[0.625rem] rounded-full border border-edge-muted pr-2">
-      <UserIcon id={props.ownerId} size="xs" />
-      shared
-    </div>
-  );
-}
-
-function GenericContentHit(props: { data: ContentHitData }) {
-  return (
-    <div class="text-sm text-ink-muted truncate flex items-center">
-      <StaticMarkdown
-        markdown={props.data.content}
-        theme={unifiedListMarkdownTheme}
-        singleLine={true}
-      />
-    </div>
-  );
-}
-
-function ChannelMessageContentHit(props: { data: ChannelContentHitData }) {
-  const [userName] = useDisplayName(props.data.senderId);
-  const formattedDate = createFormattedDate(props.data.sentAt);
-
-  return (
-    <div class="flex gap-2 items-center min-w-0">
-      <div class="flex size-5 shrink-0 items-center justify-center">
-        <UserIcon id={props.data.senderId} size="xs" />
-      </div>
-      <div class="flex gap-2 text-sm w-full min-w-0 overflow-hidden items-baseline">
-        <div class="text-sm shrink-0 truncate min-w-0 font-medium">
-          {userName()}
-        </div>
-        <div class="shrink-0 font-mono text-xs uppercase text-ink-extra-muted">
-          {formattedDate()}
-        </div>
-        <div class="text-sm text-ink-muted truncate flex items-center flex-1 min-w-0">
-          <StaticMarkdown
-            markdown={props.data.content}
-            theme={unifiedListMarkdownTheme}
-            singleLine={true}
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-// function ImportantBadge(props: { active?: boolean }) {
-//   return (
-//     <Show when={props.active}>
-//       <div class="font-mono font-medium user-select-none uppercase flex items-center text-accent bg-accent/10 p-0.5 px-2 text-[0.625rem] rounded-full border border-accent/10">
-//         <span class="@max-xl/split:hidden">Important</span>
-//         <span class="hidden @max-xl/split:block font-bold">!</span>
-//       </div>
-//     </Show>
-//   );
-// }
+    return date.toLocaleDateString('en-US', {
+      month: 'numeric',
+      day: 'numeric',
+      year: '2-digit',
+    });
+  });
 
 interface EntityProps<T extends WithNotification<EntityData>>
   extends ParentProps {
@@ -829,29 +776,71 @@ function EntityProject(props: {
   );
 }
 
-const createFormattedDate = (timestamp: number) =>
-  createMemo(() => {
-    if (timestamp < 1e12) {
-      timestamp *= 1000;
-    }
-    const date = new Date(timestamp);
-    const currentDate = new Date();
-    if (date.getDate() === currentDate.getDate()) {
-      return date.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-      });
-    }
-    if (date.getFullYear() === currentDate.getFullYear()) {
-      return date.toLocaleDateString('en-US', {
-        month: 'short',
-        day: 'numeric',
-      });
-    }
+function UnreadIndicator(props: { active?: boolean }) {
+  return (
+    <div class="flex size-4 items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        classList={{
+          'fill-accent': true,
+          'opacity-0': !props.active,
+        }}
+        viewBox="0 0 8 8"
+        width="75%"
+        height="75%"
+        fill="none"
+      >
+        <path d="M3.39622 8C3.29136 8 3.23894 7.94953 3.23894 7.84858L3.33068 5.13565L0.932129 6.58675C0.836012 6.63722 0.76174 6.6204 0.709312 6.53628L0.0801831 5.56467C0.0190178 5.47213 0.0364936 5.40063 0.132611 5.35016L2.58359 4.07571L0.09329 2.88959C-0.00282696 2.83912 -0.0246717 2.77182 0.0277557 2.6877L0.59135 1.58991C0.643778 1.49737 0.71805 1.47634 0.814167 1.52681L3.31758 2.95268L3.21272 0.151421C3.21272 0.0504735 3.26515 0 3.37 0H4.57583C4.68069 0 4.73312 0.0504735 4.73312 0.151421L4.64137 2.94006L7.14478 1.40063C7.2409 1.34175 7.3108 1.35857 7.35449 1.4511L7.97051 2.46057C8.02294 2.5531 8.00546 2.6204 7.91808 2.66246L5.40157 4L7.82633 5.18612C7.91371 5.23659 7.93556 5.30389 7.89187 5.38801L7.36759 6.4858C7.32391 6.58675 7.25837 6.60778 7.17099 6.54889L4.6938 5.13565L4.78554 7.84858C4.79428 7.94953 4.74185 8 4.62826 8H3.39622Z" />
+      </svg>
+    </div>
+  );
+}
 
-    return date.toLocaleDateString('en-US', {
-      month: 'numeric',
-      day: 'numeric',
-      year: '2-digit',
-    });
-  });
+function SharedBadge(props: { ownerId: string }) {
+  return (
+    <div class="font-mono font-medium user-select-none uppercase flex items-center text-ink-extra-muted p-0.5 gap-1 text-[0.625rem] rounded-full border border-edge-muted pr-2">
+      <UserIcon id={props.ownerId} size="xs" />
+      shared
+    </div>
+  );
+}
+
+function GenericContentHit(props: { data: ContentHitData }) {
+  return (
+    <div class="text-sm text-ink-muted truncate flex items-center">
+      <StaticMarkdown
+        markdown={props.data.content}
+        theme={unifiedListMarkdownTheme}
+        singleLine={true}
+      />
+    </div>
+  );
+}
+
+function ChannelMessageContentHit(props: { data: ChannelContentHitData }) {
+  const [userName] = useDisplayName(props.data.senderId);
+  const formattedDate = createFormattedDate(props.data.sentAt);
+
+  return (
+    <div class="flex gap-2 items-center min-w-0">
+      <div class="flex size-5 shrink-0 items-center justify-center">
+        <UserIcon id={props.data.senderId} size="xs" />
+      </div>
+      <div class="flex gap-2 text-sm w-full min-w-0 overflow-hidden items-baseline">
+        <div class="text-sm shrink-0 truncate min-w-0 font-medium">
+          {userName()}
+        </div>
+        <div class="shrink-0 font-mono text-xs uppercase text-ink-extra-muted">
+          {formattedDate()}
+        </div>
+        <div class="text-sm text-ink-muted truncate flex items-center flex-1 min-w-0">
+          <StaticMarkdown
+            markdown={props.data.content}
+            theme={unifiedListMarkdownTheme}
+            singleLine={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
