18055314d3089668386b4f0b2083b7b500406a2b
Evan Hutnik <evan.hutnik@gmail.com>
2025-12-11 10:59:13 -0500

    Add db migration

---
+++ rust/cloud-storage/macro_db_client/migrations/20251211155815_email_attachments_sfs.sql
@@ -0,0 +1,11 @@
+-- table for storing email attachment sfs data. we store videos and non-inline images in sfs so we can show thumbnail
+-- previews for them in the FE
+CREATE TABLE "email_attachments_sfs"
+(
+    id UUID PRIMARY KEY,
+    attachment_id UUID REFERENCES email_attachments (id) ON DELETE SET NULL,
+    sfs_id UUID NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_email_attachments_sfs_attachment_id ON email_attachments_sfs (attachment_id);
