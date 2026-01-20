f3fbe38785684550d06629f9cfc357f9e7c2a4a7
Rose Hall <rose@hall.ly>
2026-01-20 17:32:04 -0500

    feat(infra): use secret key names for GitHub OAuth config

--- infra/stacks/authentication-service/index.ts
+++ infra/stacks/authentication-service/index.ts
@@ -62,12 +62,7 @@ const GOOGLE_CLIENT_ID = aws.secretsmanager
   })
   .apply((secret) => secret.secretString);
 const GITHUB_CLIENT_SECRET_KEY = config.require(`github_client_secret_key`);
-const githubClientId = config.require(`github_client_id`);
-const GITHUB_CLIENT_ID = aws.secretsmanager
-  .getSecretVersionOutput({
-    secretId: githubClientId,
-  })
-  .apply((secret) => secret.secretString);
+const GITHUB_CLIENT_ID = config.require(`github_client_id`);
 
 // Using the 5 secret names
 // We need to grab their arns so we can create a policy to allow them to be retrieved by service
@@ -106,6 +101,10 @@ const githubClientSecretKeyArn: pulumi.Output<string> = aws.secretsmanager
   .getSecretVersionOutput({ secretId: GITHUB_CLIENT_SECRET_KEY })
   .apply((secret) => secret.arn);
 
+const githubClientIdArn: pulumi.Output<string> = aws.secretsmanager
+  .getSecretVersionOutput({ secretId: GITHUB_CLIENT_ID })
+  .apply((secret) => secret.arn);
+
 const STRIPE_PRICE_ID_KEY = config.require(`stripe_price_id`);
 
 const stripePriceIdArn: pulumi.Output<string> = aws.secretsmanager
@@ -139,6 +138,7 @@ const secretKeyArns = [
   pulumi.interpolate`${stripeSecretKeyArn}`,
   pulumi.interpolate`${googleClientSecretKeyArn}`,
   pulumi.interpolate`${githubClientSecretKeyArn}`,
+  pulumi.interpolate`${githubClientIdArn}`,
   pulumi.interpolate`${MACRO_API_TOKENS.macroApiTokenPublicKeyArn}`,
   pulumi.interpolate`${macroApiTokenSecretPrivateKeyArn}`,
   pulumi.interpolate`${stripeWebhookSecretKeyArn}`,
