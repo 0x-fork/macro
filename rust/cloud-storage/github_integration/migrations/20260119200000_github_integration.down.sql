1900f7d7ae3bea35d08345801e3000f8e34cd4b1
Rose Hall <rose@hall.ly>
2026-01-21 09:47:08 -0500

    chore: move migrations back to `macro_db_client`

--- rust/cloud-storage/github_integration/migrations/20260119200000_github_integration.down.sql
+++
@@ -1,8 +0,0 @@
--- Drop indexes first
-DROP INDEX IF EXISTS idx_github_links_github_username;
-DROP INDEX IF EXISTS idx_github_links_macro_id;
-DROP INDEX IF EXISTS uq_github_links_github_user_id;
-DROP INDEX IF EXISTS uq_github_links_fusionauth_user_id;
-
--- Drop table
-DROP TABLE IF EXISTS public.github_links;
