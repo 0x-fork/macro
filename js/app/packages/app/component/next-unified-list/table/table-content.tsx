29de85700f3e6eed3dcd0946067df1248697d76d
Rahul <43100342+dev-rb@users.noreply.github.com>
2026-01-26 14:41:57 -0500

    Make overscan and itemSize adjustable from props

--- js/app/packages/app/component/next-unified-list/table/table-content.tsx
+++ js/app/packages/app/component/next-unified-list/table/table-content.tsx
@@ -4,9 +4,13 @@ import { cn } from '@ui/utils/classname';
 import { type Accessor, createMemo, type JSX } from 'solid-js';
 import { VList } from 'virtua/solid';
 
+const DEFAULT_ITEM_SIZE = 50;
+const DEFAULT_OVERSCAN = 5;
+
 interface TableContentProps<TData> {
   class?: string;
   itemSize?: number;
+  overscan?: number;
   children: (row: Row<TData>, index: Accessor<number>) => JSX.Element;
 }
 
@@ -14,6 +18,10 @@ interface TableContentProps<TData> {
 export function TableContent<TData>(props: TableContentProps<TData>) {
   const context = useTable<TData>();
   const rows = createMemo(() => context.table.getRowModel().rows);
+
+  const itemSize = createMemo(() => props.itemSize ?? DEFAULT_ITEM_SIZE);
+  const overscan = createMemo(() => props.overscan ?? DEFAULT_OVERSCAN);
+
   return (
     <div
       class={cn(
@@ -21,7 +29,11 @@ export function TableContent<TData>(props: TableContentProps<TData>) {
         props.class
       )}
     >
-      <VList data={rows()} itemSize={50} bufferSize={10 * 50}>
+      <VList
+        data={rows()}
+        itemSize={itemSize()}
+        bufferSize={overscan() * itemSize()}
+      >
         {(row, i) => props.children(row, i)}
       </VList>
     </div>
