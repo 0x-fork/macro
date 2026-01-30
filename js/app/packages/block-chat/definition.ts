a26b7b44ffe7c5255bf6242a6266eba1bed963da
ehayes <eqhayes@protonmail.com>
2026-01-30 15:42:22 -0500

    ts-fix

--- js/app/packages/block-chat/definition.ts
+++ js/app/packages/block-chat/definition.ts
@@ -42,7 +42,6 @@ export const definition = defineBlock({
           owner: chat.chat.userId,
           createdAt: chat.chat.createdAt,
           updatedAt: chat.chat.updatedAt,
-          deletedAt: null,
           fileType: 'chat' as any,
         } satisfies DocumentMetadata,
       });
