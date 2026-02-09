f46b8b1354b287598b60fe674d0f769649b90827
ehayes <eqhayes@protonmail.com>
2026-02-09 17:03:40 -0500

    clippy

--- rust/cloud-storage/models_document/src/response/mod.rs
+++ rust/cloud-storage/models_document/src/response/mod.rs
@@ -255,6 +255,9 @@ pub struct CreateDocumentRequest {
     /// md.
     #[serde(default)]
     pub is_task: bool,
+    /// Whether to add a viewed_at record for this document upon creation.
+    #[serde(default)]
+    pub skip_history: bool,
 }
 
 /// Presigned URL for document parts.
