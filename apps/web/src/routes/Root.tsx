200307504a9ded62274e0de5ee3a9cffdb3b32ee
dev-rb <43100342+dev-rb@users.noreply.github.com>
2026-07-20 14:23:51 -0400

    feat(quick-access): use graphql cache instead of history (#4982)
    Co-authored-by: Sean Aye <hello@seanaye.ca>

--- apps/web/src/routes/Root.tsx
+++ apps/web/src/routes/Root.tsx
@@ -77,7 +77,6 @@ import {
   useUserInfoQuery,
 } from '@queries/auth/user-info';
 import { useChatRenameWebsocketSync } from '@queries/chat';
-import { prefetchHistory } from '@queries/history/history';
 import { QuerySyncProvider } from '@queries/sync/SyncProvider';
 import { MutationUndoProvider } from '@queries/undo';
 import { useReopenTrackedEntitiesOnReconnect } from '@service-connection/client';
@@ -129,7 +128,6 @@ function useSyncLoginCookie() {
 
 const rootPreload: RoutePreloadFunc = async (args) => {
   await prefetchUserInfo();
-  prefetchHistory();
 
   // even though we are using the transformUrl prop, we may still need to replace the url in the history
   const url = new URL(window.location.href);
