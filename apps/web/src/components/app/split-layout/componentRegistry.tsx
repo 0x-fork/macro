6b28bce9e9fbcde347a26942400620a076a9ea53
Eric Hayes <eqhayes@protonmail.com>
2026-08-04 12:25:17 -0400

    Fix skill listing soup filter and add create-skill dialog to the skills menu

--- apps/web/src/components/app/split-layout/componentRegistry.tsx
+++ apps/web/src/components/app/split-layout/componentRegistry.tsx
@@ -12,6 +12,7 @@ import { useAnalytics } from '@app/lib/analytics/analytics-context';
 import { globalSplitManager } from '@app/signal/splitLayout';
 import { ChannelCompose } from '@block-channel/component/Compose';
 import { EmailCompose } from '@block-email/component/compose/Compose';
+import { ComposeSkill } from '@block-md/component/ComposeSkill';
 import { ComposeTask } from '@block-md/component/ComposeTask';
 import {
   CRM_VIEW_URL_PARAM,
@@ -472,6 +473,10 @@ registerComponent('email-compose', (params) => {
 registerComponent('task-compose', (params) => {
   usePageViewTracking('task-compose');
   return <ComposeTask {...params} />;
+});
+registerComponent('skill-compose', (params) => {
+  usePageViewTracking('skill-compose');
+  return <ComposeSkill {...params} />;
 });
 registerComponent(
   'import-linear',
