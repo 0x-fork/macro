b4725b0009048254876710fc4e873a2dab6e305f
Wolf Mermelstein <wolf@404wolf.com>
2026-08-17 19:51:17 -0400

    chore(sdk): hydrate inbound events, cleanup bot channel sending (#5689)

--- packages/sdk/src/coverage/skipped.ts
+++ packages/sdk/src/coverage/skipped.ts
@@ -272,7 +272,6 @@ export const storageExcluded = [
   'jobProcessingResultHandler',
   'leaveOrEndCall',
   'patchViewHandler',
-  'postChannelBotWebhook',
   'postChannelMessages',
   'postItemsSoup',
   'postItemsSoupAst',
