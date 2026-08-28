3ebb3e9c6b0e14083a38384c21287d3782bce4dd
Sean Aye <hello@seanaye.ca>
2026-08-28 16:17:17 +0000

    Seanaye/fix/refetch on create (#5965)

--- apps/web/src/features/next-soup/soup-view/soup-view-context.tsx
+++ apps/web/src/features/next-soup/soup-view/soup-view-context.tsx
@@ -1001,6 +1001,7 @@ export const SoupViewContextProvider: FlowComponent<
       return {
         enabled: enabled() && !search.isSearching(),
         showSupportedForeignEntities: showSupportedForeignEntitiesFF().enabled,
+        onBeforeGraphqlRefresh: () => groupQueries.resetToInitialPage(),
         meta: {
           itemFilter: (item) => soupItemMatchesActiveFilters(item, view),
         },
